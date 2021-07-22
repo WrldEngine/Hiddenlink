da3f7039b000775e3bf5b27f3fea4d79874ccf72
Callisto <84636918+WrldEngine@users.noreply.github.com>
2021-07-22 14:53:24 +0500

    Update hlink.py

--- hlink.py
+++ hlink.py
@@ -1,9 +1,23 @@
 # Pulatov Kamran(c)
 import requests
 from bs4 import BeautifulSoup
-url = input("Enter a url: ")
+GREEN='\033[32m'
+RED='\033[31m'
+banner = '''
+
+██╗░░██╗██╗██████╗░██████╗░███████╗███╗░░██╗  ██╗░░░░░██╗███╗░░██╗██╗░░██╗
+██║░░██║██║██╔══██╗██╔══██╗██╔════╝████╗░██║  ██║░░░░░██║████╗░██║██║░██╔╝
+███████║██║██║░░██║██║░░██║█████╗░░██╔██╗██║  ██║░░░░░██║██╔██╗██║█████═╝░
+██╔══██║██║██║░░██║██║░░██║██╔══╝░░██║╚████║  ██║░░░░░██║██║╚████║██╔═██╗░
+██║░░██║██║██████╔╝██████╔╝███████╗██║░╚███║  ███████╗██║██║░╚███║██║░╚██╗
+╚═╝░░╚═╝╚═╝╚═════╝░╚═════╝░╚══════╝╚═╝░░╚══╝  ╚══════╝╚═╝╚═╝░░╚══╝╚═╝░░╚═╝
+Author: Pulatov Kamran
+Pulatov Kamran(C)
+'''
+print(RED + banner)
+url = input(GREEN + "Enter a url: ")
 HEADERS = {'user-agent': 'your user-agent'}
-response = requests.get(URL, headers=HEADERS)
+response = requests.get(url, headers=HEADERS)
 bs = BeautifulSoup(response.content, 'html.parser')
 for url in bs.find_all('a'):
     print(url.get('href'))
